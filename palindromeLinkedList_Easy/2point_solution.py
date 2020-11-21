65471bdd8ec6e7e75c9fd4b2c0d15215540866a5
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-11-20 23:40:52 -0800

    add comments to 2point_solution

--- palindromeLinkedList_Easy/2point_solution.py
+++ palindromeLinkedList_Easy/2point_solution.py
@@ -15,12 +15,13 @@ class Solution:
             return prev_node
         fast = head
         slow = head
+        # Goes through list until lit reaches end, so slow is the next-to-last value
         while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
         fast = head
         slow = reverse(slow)
-
+        # This then checks against the reversed list
         while slow is not None:
             if slow.val != fast.val:
                 return False
